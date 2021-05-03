2c17b63ea251ab943d4566e1b069c83e6c5330ae
Ruilin Li <ruilinli@stanford.edu>
2021-05-02 20:25:00 -0700

    get starter code

---
+++ basic_profile.py
@@ -0,0 +1,32 @@
+from resnet import se_resnet9_fixup
+from torch.utils.tensorboard import SummaryWriter
+import torch
+import torch.autograd.profiler as profiler
+
+device = torch.device("cuda")
+net = se_resnet9_fixup(3, 64, 16).to(device)
+
+
+inp = torch.rand((2048, 3, 64, 64), dtype=torch.float16, device=device)
+
+
+# Backward
+# with torch.cuda.amp.autocast():
+#     out = net(inp)
+#     loss = torch.nn.MSELoss()
+#     target = torch.rand_like(out, device=device) # fake target
+#     output = loss(out, target)
+#     output.backward(retain_graph=True)
+#     with profiler.profile(record_shapes=True, use_cuda=True) as prof:
+#         # out = net(inp)
+#         output = loss(out, target)
+#         output.backward()
+
+# Forward
+with torch.cuda.amp.autocast():
+    # warm-up
+    out = net(inp)
+    with profiler.profile(record_shapes=True, use_cuda=True) as prof:
+        out = net(inp)
+        
+print(prof.key_averages().table(sort_by='cuda_time_total'))
